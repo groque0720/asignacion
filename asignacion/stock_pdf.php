00904218ebcb48f59e06bf17cbc67ab6bddacc58
Gómez Roque <gomezroque@hotmail.com>
2019-12-22 10:55:49 -0300

    R221220191055

--- asignacion/stock_pdf.php
+++ asignacion/stock_pdf.php
@@ -13,7 +13,7 @@ class PDF extends FPDF
 		if ($this->PageNo()==1) {
 			$this->SetFont('Arial','B',8);
 			$this->Cell(45,5,'DERKA Y VARGAS S. A.',0,0,'C');
-			$this->Cell(100,5,utf8_decode('PLANILLA DE STOCK'),0,0,'C');
+			$this->Cell(100,5,('PLANILLA DE STOCK'),0,0,'C');
 			$this->Cell(0,5,cambiarFormatoFecha(date('Y-m-d')).' - '. strftime("%H:%M"),0,0,'C');
 			$this->Ln();
 			$this->Cell(0,0,'',1,0,'C');
@@ -23,7 +23,7 @@ class PDF extends FPDF
 
 		$this->SetFont('Arial','I',6.5);
 		$this->SetFont('');
-		$this->Cell(0,5,utf8_decode('Página').$this->PageNo().'/{nb}',0,0,'R');
+		$this->Cell(0,5,('Página').$this->PageNo().'/{nb}',0,0,'R');
 		$this->Ln();
 
 
@@ -112,6 +112,14 @@ $SQL="SELECT * FROM grupos WHERE idgrupo<>1 AND cerokilometro = 1 AND posicion >
 $grupos = mysqli_query($con, $SQL);
 $cant_grupo=0;
 
+	for ($i=0; $i < 10; $i++) {
+		$stock_ant_g[$i]['cant'] = 0;
+		$total_m[$i]['cant'] = 0;
+		$total_g[$i]['cant'] = 0;
+		$total_r_m[$i]['cant'] = 0;
+		$total_r_g[$i]['cant'] = 0;
+	}
+
 while ($grupo=mysqli_fetch_array($grupos)) {
 
 	if ($grupo['agrupar']==1) {
@@ -125,6 +133,8 @@ while ($grupo=mysqli_fetch_array($grupos)) {
 
 	$SQL="SELECT * FROM modelos WHERE posicion > 0 AND activo = 1 AND idgrupo = ".$grupo['idgrupo']." ORDER BY posicion";
 	$modelos=mysqli_query($con, $SQL);
+
+
 
 	while ($modelo=mysqli_fetch_array($modelos)) {
 
