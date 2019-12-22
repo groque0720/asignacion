9ae64bfa2f389d679aa469bd6969390151da434d
Gómez Roque <gomezroque@hotmail.com>
2019-12-22 08:58:56 -0300

    R221220190858

--- asignacion/prueba_pdf.php
+++ asignacion/prueba_pdf.php
@@ -7,7 +7,7 @@ require('fpdf/fpdf.php');
 	mysqli_query($con,"SET NAMES 'utf8'");
 	extract($_POST);
 
-		//cargo en un arreglo todos los meses que ocuparia en la tabla.
+	//cargo en un arreglo todos los meses que ocuparia en la tabla.
 		$SQL="SELECT * FROM meses";
 		$meses=mysqli_query($con, $SQL);
 		$i=1;
@@ -15,6 +15,57 @@ require('fpdf/fpdf.php');
 			$mes_a[$i]['mes']= $mes['mes'];
 			$i++;
 		}
+	//fin de carga de meses.
+	//
+	//cargo en arreglo los colores de la tabla
+	$SQL="SELECT * FROM colores ORDER BY color";
+	$colores=mysqli_query($con, $SQL);
+	$color_a[0]['color']= '-';
+	$i=1;
+	while ($color=mysqli_fetch_array($colores)) {
+		$color_a[$color['idcolor']]['color']= $color['color'];
+		$i++;
+	}
+	//fin de carga de colores
+	//
+	//	cargo los destinos de unidad
+	$SQL="SELECT * FROM sucursales";
+	$sucursales=mysqli_query($con, $SQL);
+	$sucursal_a[0]['sucres']= '-';
+	$i=1;
+	while ($sucursal=mysqli_fetch_array($sucursales)) {
+		$sucursal_a[$i]['sucres']= $sucursal['sucres'];
+		$i++;
+	}
+	//fin de carga de sucursales
+	//
+	//
+	$SQL="SELECT * FROM usuarios WHERE idperfil = 3";
+	$usuarios=mysqli_query($con, $SQL);
+	$usuario_a[1]['nombre']= '-';
+	$i=1;
+	while ($usuario=mysqli_fetch_array($usuarios)) {
+		$usuario_a[$usuario['idusuario']]['nombre']= $usuario['nombre'];
+		$i++;
+	}
+
+	$SQL="SELECT * FROM grupos WHERE activo = 1";
+	$grupos=mysqli_query($con, $SQL);
+	$grupo_a[]['grupo']= '-';
+	$i=1;
+	while ($grupo=mysqli_fetch_array($grupos)) {
+		$grupo_a[$grupo['idgrupo']]['grupo']= $grupo['grupo'];
+		$i++;
+	}
+
+	$SQL="SELECT * FROM modelos WHERE activo = 1";
+	$modelos=mysqli_query($con, $SQL);
+	$modelos_a[]['modelo']= '-';
+	$i=1;
+	while ($modelo=mysqli_fetch_array($modelos)) {
+		$modelo_a[$modelo['idmodelo']]['modelo']= $modelo['modelo'];
+		$i++;
+	}
 
 $pdf = new FPDF();
 $pdf->AddPage();
