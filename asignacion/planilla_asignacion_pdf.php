550be98542e1c2f433417f8eeca5994bfe5fc53c
Gómez Roque <gomezroque@hotmail.com>
2019-12-22 09:35:14 -0300

    954852

--- asignacion/planilla_asignacion_pdf.php
+++ asignacion/planilla_asignacion_pdf.php
@@ -77,7 +77,8 @@ class PDF extends FPDF
 			$this->SetFont('Arial','B',10);
 			$this->Cell(60,5,'DERKA Y VARGAS S. A.',0,0,'L');
 			$this->Cell(150,5,utf8_decode('PLANILLA DE ASIGNACIÓN'),0,0,'C');
-			$this->Cell(0,5,cambiarFormatoFecha(date('Y-m-d')).' - '. strftime("%H:%M"),0,0,'R');
+			$this->Cell(0,5,'-',0,0,'R');
+			// $this->Cell(0,5,cambiarFormatoFecha(date('Y-m-d')).' - '. strftime("%H:%M"),0,0,'R');
 			$this->Ln();
 			$this->Cell(0,0,'',1,0,'C');
 			$this->Ln();
