9374ef7df6f1c59418a9d0fd91251d6b7a2c4611
Gómez Roque <gomezroque@hotmail.com>
2019-12-22 08:57:49 -0300

    R221220190857

--- asignacion/prueba_pdf.php
+++ asignacion/prueba_pdf.php
@@ -7,6 +7,15 @@ require('fpdf/fpdf.php');
 	mysqli_query($con,"SET NAMES 'utf8'");
 	extract($_POST);
 
+		//cargo en un arreglo todos los meses que ocuparia en la tabla.
+		$SQL="SELECT * FROM meses";
+		$meses=mysqli_query($con, $SQL);
+		$i=1;
+		while ($mes=mysqli_fetch_array($meses)) {
+			$mes_a[$i]['mes']= $mes['mes'];
+			$i++;
+		}
+
 $pdf = new FPDF();
 $pdf->AddPage();
 $pdf->SetFont('Arial','B',16);
