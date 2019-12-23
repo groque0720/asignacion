b3ef2535eacad77e393e1f624783910c169ba44e
Gómez Roque <gomezroque@hotmail.com>
2019-12-23 17:08:01 -0300

    R231220191707

--- asignacion/excel_planilla_asignacion_prueba.php
+++ asignacion/excel_planilla_asignacion_prueba.php
@@ -1,6 +1,10 @@
 ﻿<?php
-header('Content-type: application/vnd.ms-excel;charset=iso-8859-15');
-header('Content-Disposition: attachment; filename=nombre_archivo.xlsx');
+// header('Content-type: application/vnd.ms-excel;charset=iso-8859-15');
+// header('Content-Disposition: attachment; filename=nombre_archivo.xlsx');
+
+header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+header('Content-Disposition: attachment;filename="PlanillaAsignación_'.date("d-m-Y").'_'.date("H").date("i").date("s").'".xls"');
+header('Cache-Control: max-age=0');
 ?>
 
 <table border="1" cellpadding="2" cellspacing="0" width="100%">
@@ -21,3 +25,7 @@ header('Content-Disposition: attachment; filename=nombre_archivo.xlsx');
         <td>Datos</td>
     </tr>
 </table>
+
+<?php
+ob_end_clean();
+ ?>
