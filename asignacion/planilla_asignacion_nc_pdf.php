ad7e5a12ba26be5c5d963218f40969b40c88ada0
Gómez Roque <gomezroque@hotmail.com>
2019-12-26 13:19:03 -0300

    R261220191318

--- asignacion/planilla_asignacion_nc_pdf.php
+++ asignacion/planilla_asignacion_nc_pdf.php
@@ -76,7 +76,7 @@ function Header()
 	if ($this->PageNo()==1) {
 		$this->SetFont('Arial','B',10);
 		$this->Cell(60,5,'DERKA Y VARGAS S. A.',0,0,'L');
-		$this->Cell(150,5,utf8_decode('PLANILLA DE ASIGNACIÓN - RESERVAS NO CONFIRMADAS'),0,0,'C');
+		$this->Cell(150,5,('PLANILLA DE ASIGNACION - RESERVAS NO CONFIRMADAS'),0,0,'C');
 		$this->Cell(0,5,cambiarFormatoFecha(date('Y-m-d')).' - '. strftime("%H:%M"),0,0,'R');
 		$this->Ln();
 		$this->Cell(0,0,'',1,0,'C');
@@ -84,11 +84,11 @@ function Header()
 	}
 $this->SetFont('Arial','I',7);
 $this->SetFont('');
-$this->Cell(0,5,utf8_decode('Página').$this->PageNo().'/{nb}',0,0,'R');
+$this->Cell(0,5,('Pag.').$this->PageNo().'/{nb}',0,0,'R');
 $this->Ln();
 $this->Cell(9,5,'Nro Un.',0,0,'C');
 $this->Cell(15,5,'Mes',0,0,'C');
-$this->Cell(10,5,utf8_decode('Año'),0,0,'C');
+$this->Cell(10,5,('Ano'),0,0,'C');
 $this->Cell(18,5,'Nro Orden',0,0,'C');
 $this->Cell(10,5,'Interno',0,0,'C');
 $this->Cell(12,5,'Despacho',0,0,'C');
@@ -124,22 +124,22 @@ $pdf->AliasNbPages();
 $pdf->AddPage('L','A4');
 $pdf->SetLineWidth(0.1);
 $pdf->SetDrawColor(184, 184, 184);
-$pdf->SetAutoPageBreak(auto,5);
+$pdf->SetAutoPageBreak(true,5);
 
 $SQL="SELECT * FROM grupos WHERE activo = 1 AND cerokilometro = 1 AND posicion>0 ORDER BY posicion";
 $grupos=mysqli_query($con, $SQL);
 
 	while ($grupo=mysqli_fetch_array($grupos)) {
-		// $pdf->SetFont('Arial','B',10); 
+		// $pdf->SetFont('Arial','B',10);
 		// $pdf->SetFont('');
 		// $pdf->Cell(0,5,$grupo_a[$grupo['idgrupo']]['grupo'],1,1,'C');
-		
+
 
 		$SQL="SELECT * FROM modelos WHERE activo = 1 AND idgrupo=".$grupo['idgrupo']." ORDER BY posicion" ;
 		$modelos=mysqli_query($con, $SQL);
 
 		while ($modelo=mysqli_fetch_array($modelos)) {
-			
+
 
 			$SQL="SELECT * FROM asignaciones WHERE reservada = 1 AND estado_reserva = 0 AND entregada = 0 AND id_modelo = ". $modelo['idmodelo'] ." ORDER BY año, id_mes, nro_orden, nro_unidad";
 			$unidades = mysqli_query($con, $SQL);
@@ -156,7 +156,7 @@ $grupos=mysqli_query($con, $SQL);
 			$pdf->SetFont('Arial','B',6.5);
 			$pdf->SetFont('');
 
-			
+
 			while ($unidad=mysqli_fetch_array($unidades)) {
 
 				if ($unidad['reservada']==1 AND $unidad['estado_reserva']==0 ) {
@@ -167,26 +167,26 @@ $grupos=mysqli_query($con, $SQL);
 				}
 
 
-				$pdf->Cell(9,5,utf8_decode($unidad['nro_unidad']),1,0,'C');
-				$pdf->Cell(15,5,utf8_decode($mes_a[$unidad['id_mes']]['mes']),1,0,'C');
-				$pdf->Cell(10,5,utf8_decode($unidad['año']),1,0,'C');
-				$pdf->Cell(18,5,utf8_decode($unidad['nro_orden']),1,0,'C');
-				$pdf->Cell(10,5,utf8_decode($unidad['interno']),1,0,'C');
+				$pdf->Cell(9,5,($unidad['nro_unidad']),1,0,'C');
+				$pdf->Cell(15,5,($mes_a[$unidad['id_mes']]['mes']),1,0,'C');
+				$pdf->Cell(10,5,($unidad['año']),1,0,'C');
+				$pdf->Cell(18,5,($unidad['nro_orden']),1,0,'C');
+				$pdf->Cell(10,5,($unidad['interno']),1,0,'C');
 				$pdf->Cell(12,5,cambiarFormatoFecha($unidad['fec_despacho']),1,0,'C');
 				$pdf->Cell(12,5,cambiarFormatoFecha($unidad['fec_arribo']),1,0,'C');
-				$pdf->Cell(47,5,utf8_decode($grupo_a[$unidad['id_grupo']]['grupo']." ".$modelo_a[$unidad['id_modelo']]['modelo']),1,0,'C');
-				$pdf->Cell(13,5,utf8_decode($unidad['chasis']),1,0,'C');
-				$pdf->Cell(35,5,utf8_decode($color_a[$unidad['color_uno']]['color']." - ".$color_a[$unidad['color_dos']]['color']." - ".$color_a[$unidad['color_tres']]['color']),1,0,'C');
-				$pdf->Cell(12,5,utf8_decode($color_a[$unidad['id_color']]['color']),1,0,'C');
+				$pdf->Cell(47,5,($grupo_a[$unidad['id_grupo']]['grupo']." ".$modelo_a[$unidad['id_modelo']]['modelo']),1,0,'C');
+				$pdf->Cell(13,5,($unidad['chasis']),1,0,'C');
+				$pdf->Cell(35,5,($color_a[$unidad['color_uno']]['color']." - ".$color_a[$unidad['color_dos']]['color']." - ".$color_a[$unidad['color_tres']]['color']),1,0,'C');
+				$pdf->Cell(12,5,($color_a[$unidad['id_color']]['color']),1,0,'C');
 
 				if ($unidad['fec_arribo']!='' AND $unidad['fec_arribo']!=null) {
-					$pdf->Cell(9,5,utf8_decode($sucursal_a[$unidad['id_ubicacion']]['sucres']),1,0,'C');
+					$pdf->Cell(9,5,($sucursal_a[$unidad['id_ubicacion']]['sucres']),1,0,'C');
 				 }else{
-					$pdf->Cell(9,5,utf8_decode($sucursal_a[$unidad['id_sucursal']]['sucres']),1,0,'C');
+					$pdf->Cell(9,5,($sucursal_a[$unidad['id_sucursal']]['sucres']),1,0,'C');
 				}
 				//resalto la fuente de cancelación - Pedido Don Vargas
 				$pdf->SetFont('Arial','B',7.5);
-				
+
 				if ($unidad['cancelada']==1) { $can= 'Si';}else{$can= '';}
 				if ($unidad['patentada']==1) { $pat= '/Si';}else{$pat= '';}
 
@@ -199,13 +199,13 @@ $grupos=mysqli_query($con, $SQL);
 					$pdf->SetFont('Arial','B',6.5);
 					$pdf->SetFont('');
 				}
-				$pdf->Cell(38,5,utf8_decode($unidad['cliente']),1,0,'L');
-				$pdf->Cell(17,5,utf8_decode($usuario_a[$unidad['id_asesor']]['nombre']),1,0,'C');
+				$pdf->Cell(38,5,($unidad['cliente']),1,0,'L');
+				$pdf->Cell(17,5,($usuario_a[$unidad['id_asesor']]['nombre']),1,0,'C');
 				$pdf->Cell(12,5,cambiarFormatoFecha($unidad['fec_reserva']),1,0,'C');
 				$pdf->Ln();
 			}
 			$pdf->Ln(2);
-		}	
+		}
 	}
 
 $pdf->Output('PlanilaAsignacion_'.cambiarFormatoFecha(date('Y-m-d')).'Hs'. strftime("%H:%M").'.pdf','I');
