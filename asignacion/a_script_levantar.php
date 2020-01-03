aa5551277083f68ed0004c1ade2c97bfe7106a63
Gómez Roque <32652492+groque0720@users.noreply.github.com>
2020-01-03 14:10:06 -0300

    R030120201410

--- asignacion/a_script_levantar.php
+++ asignacion/a_script_levantar.php
@@ -12,7 +12,7 @@
 	mysqli_query($con, $SQL);
 
 
-	$SQL="INSERT INTO sesiones (id_usuario, nombre, fecha, hora, latitud, longitud, ip) VALUES (1,'pepe','".date("Y-m-d")."',now(),'','',5)";
+		$SQL="INSERT INTO sesiones (id_usuario, nombre, fecha, hora, latitud, longitud, ip) VALUES (1,'pepe','".date("Y-m-d")."','".date( 'H:i:s')."','','',5)";
 	mysqli_query($con, $SQL);
 
 	if ($nro_dia!=0) {
@@ -41,7 +41,7 @@
 					$SQL="INSERT INTO a_modificaciones (modelo_activo, fecha) VALUES(".$unidad['id_modelo'].",'".date("Y-m-d")."')";
 					mysqli_query($con, $SQL);
 
-					$SQL="INSERT INTO asignaciones_levantadas (nro_unidad, id_mes, año, id_grupo, id_modelo, fec_reserva, fec_alta, hora, cliente, id_asesor, hora_alta) VALUES (".$unidad['nro_unidad'].", ".$unidad['id_mes'].", ".$unidad['año'].", ".$unidad['id_grupo'].", ".$unidad['id_modelo'].", '".$unidad['fec_reserva']."', '".date("Y-m-d")."', '".$unidad['hora']."', '".$unidad['cliente']."', ".$unidad['id_asesor'].", now())";
+					$SQL="INSERT INTO asignaciones_levantadas (nro_unidad, id_mes, año, id_grupo, id_modelo, fec_reserva, fec_alta, hora, cliente, id_asesor, hora_alta) VALUES (".$unidad['nro_unidad'].", ".$unidad['id_mes'].", ".$unidad['año'].", ".$unidad['id_grupo'].", ".$unidad['id_modelo'].", '".$unidad['fec_reserva']."', '".date("Y-m-d")."', '".$unidad['hora']."', '".$unidad['cliente']."', ".$unidad['id_asesor'].", '".date( 'H:i:s')."')";
 					mysqli_query($con, $SQL);
 					echo $unidad['cliente'];
 					$SQL="UPDATE asignaciones SET ";
@@ -72,7 +72,7 @@
 
 					echo $usado['nro_unidad'].'-'.$usado['cliente'];
 
-					$SQL="INSERT INTO asignaciones_usados_levantadas (nro_unidad, id_estado, interno, vehiculo, cliente, id_asesor, fec_reserva, hora, fec_alta, hora_alta, asesor) VALUES (".$usado['nro_unidad'].", ".$usado['id_estado'].", ".$usado['interno'].", '".$usado['vehiculo']."', '".$usado['cliente']."', ".$usado['id_asesor'].", '".$usado['fec_reserva']."', '".$usado['hora']."', '".date("Y-m-d")."', now(), '".$usado['asesor']."')";
+					$SQL="INSERT INTO asignaciones_usados_levantadas (nro_unidad, id_estado, interno, vehiculo, cliente, id_asesor, fec_reserva, hora, fec_alta, hora_alta, asesor) VALUES (".$usado['nro_unidad'].", ".$usado['id_estado'].", ".$usado['interno'].", '".$usado['vehiculo']."', '".$usado['cliente']."', ".$usado['id_asesor'].", '".$usado['fec_reserva']."', '".$usado['hora']."', '".date("Y-m-d")."','".date( 'H:i:s')."', '".$usado['asesor']."')";
 					mysqli_query($con,$SQL);
 
 					$SQL="UPDATE asignaciones_usados SET ";
@@ -103,7 +103,7 @@
 								$SQL="INSERT INTO a_modificaciones (modelo_activo, fecha) VALUES(".$unidad['id_modelo'].",'".date("Y-m-d")."')";
 								mysqli_query($con, $SQL);
 
-								$SQL="INSERT INTO asignaciones_levantadas (nro_unidad, id_mes, año, id_grupo, id_modelo, fec_reserva, fec_alta, hora, cliente, id_asesor, hora_alta) VALUES (".$unidad['nro_unidad'].", ".$unidad['id_mes'].", ".$unidad['año'].", ".$unidad['id_grupo'].", ".$unidad['id_modelo'].", '".$unidad['fec_reserva']."', '".date("Y-m-d")."', '".$unidad['hora']."', '".$unidad['cliente']."', ".$unidad['id_asesor'].", now())";
+								$SQL="INSERT INTO asignaciones_levantadas (nro_unidad, id_mes, año, id_grupo, id_modelo, fec_reserva, fec_alta, hora, cliente, id_asesor, hora_alta) VALUES (".$unidad['nro_unidad'].", ".$unidad['id_mes'].", ".$unidad['año'].", ".$unidad['id_grupo'].", ".$unidad['id_modelo'].", '".$unidad['fec_reserva']."', '".date("Y-m-d")."', '".$unidad['hora']."', '".$unidad['cliente']."', ".$unidad['id_asesor'].", '".date( 'H:i:s')."')";
 								mysqli_query($con, $SQL);
 								echo $unidad['cliente'];
 								$SQL="UPDATE asignaciones SET ";
