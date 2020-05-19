649354c7f6b6dc0498f2a7b65c9022b843bc14a4
Gómez Roque <32652492+groque0720@users.noreply.github.com>
2020-05-19 17:48:36 -0300

    R190520201748

--- config/config_mysql.php
+++ config/config_mysql.php
@@ -8,21 +8,21 @@
 	// //base de datos
 	// define('DB','asignacion');
 
-	//servidor
-	define('HOST','localhost');
-	//usuario bd
-	define('USER','usuario_remoto');
-	//pass
-	define('PASS','Belgrano872');
-	//base de datos
-	define('DB','asignacion');
-
 	// //servidor
 	// define('HOST','localhost');
 	// //usuario bd
-	// define('USER','usernavicat');
+	// define('USER','usuario_remoto');
 	// //pass
-	// define('PASS','Belgrano872!');
+	// define('PASS','Belgrano872');
 	// //base de datos
 	// define('DB','asignacion');
+
+	//servidor
+	define('HOST','localhost');
+	//usuario bd
+	define('USER','usernavicat');
+	//pass
+	define('PASS','Belgrano872!');
+	//base de datos
+	define('DB','asignacion');
 ?>
