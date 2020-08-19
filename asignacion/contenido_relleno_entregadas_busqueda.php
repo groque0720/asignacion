c6d36de7ef9ebca3270dca384879642cad073efd
Gómez Roque <32652492+groque0720@users.noreply.github.com>
2020-08-19 12:32:02 -0300

    R190820201231

--- asignacion/contenido_relleno_entregadas_busqueda.php
+++ asignacion/contenido_relleno_entregadas_busqueda.php
@@ -1,4 +1,4 @@
-﻿  <?php 
+﻿  <?php
   include("funciones/func_mysql.php");
 	conectar();
 	mysqli_query($con,"SET NAMES 'utf8'");
@@ -6,20 +6,28 @@
 
 	$cadena='';
 
-	if ($mes_desde!=0) {
-		$cadena.=" MONTH(fec_entrega) >=".$mes_desde;
-	}
-	if ($año_desde!='' AND $año_desde>=2016) {
-		$cadena.=" AND YEAR(fec_entrega) >=".$año_desde;
-	}
-	if ($mes_hasta!=0) {
-		$cadena.=" OR (MONTH(fec_entrega) <=".$mes_hasta;
-	}
-	if ($año_hasta!='' AND $año_hasta>=2016) {
-		$cadena.=" AND YEAR(fec_entrega) <=".$año_hasta.") ";
-	}
+	// if ($mes_desde!=0) {
+	// 	$cadena.=" (MONTH(fec_entrega) >=".$mes_desde;
+	// }
+	// if ($año_desde!='' AND $año_desde>=2016) {
+	// 	$cadena.=" AND YEAR(fec_entrega) >=".$año_desde;
+	// }
+	// if ($mes_hasta!=0) {
+	// 	$cadena.=" )AND ((MONTH(fec_entrega) <=".$mes_hasta;
+	// }
+	// if ($año_hasta!='' AND $año_hasta>=2016) {
+	// 	$cadena.=" AND YEAR(fec_entrega) <=".$año_hasta.") )";
+	// }
+
+
+	$ultimo_dia_mes_hasta = cal_days_in_month(CAL_GREGORIAN, $mes_hasta, $año_hasta);
+	$fec_ini = $año_desde.'/'.$mes_desde.'/01';
+	$fec_fin = $año_hasta.'/'.$mes_hasta.'/'.$ultimo_dia_mes_hasta;
+
+	$cadena .= " fec_entrega >= '$fec_ini' AND fec_entrega <= '$fec_fin' ";
+
+
 $SQL="SELECT * FROM asignaciones WHERE entregada = 1 AND".$cadena." ORDER BY fec_entrega DESC";
-//echo $SQL;
 $unidades = mysqli_query($con, $SQL);
 include('contenido_relleno_entregadas_cuerpo.php');
  ?>
