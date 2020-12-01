b9711bb897a090bd0c5d95c2ab45507c713924be
Gómez Roque <32652492+groque0720@users.noreply.github.com>
2020-12-01 19:05:14 -0300

    R01120201905

---
+++ asignacion/index_.php
@@ -0,0 +1,37 @@
+﻿
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Asignación TASA</title>
+	<link rel="shortcut icon" type="image/x-icon" href="dyv.ico" />
+	<link rel="stylesheet" href="css/estilo.css">
+	<style>
+		.cuerpo {
+			width: 90%;
+			margin: auto;
+			margin-top: 40px;
+		}
+
+		.img {
+			margin: auto;
+			text-align: center;
+			width: 50%;
+		}
+		.imagen{
+			width: 70%;
+		}
+	</style>
+
+</head>
+<body>
+<h1><center>Realizando Asignación Diciembre 2020</center></h1>
+
+	<div class="cuerpo">
+		<div class="img">
+			<img class="imagen" src="imagenes/webmant.jpg" alt="en mantenimeinto">
+		</div>
+	</div>
+
+</body>
+</html>
