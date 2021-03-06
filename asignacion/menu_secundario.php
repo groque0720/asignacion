df8ddb0f276f09f1928b0bf2114dc1d747cf8473
Gómez Roque <32652492+groque0720@users.noreply.github.com>
2021-03-06 11:44:54 -0300

    R060320211144

--- asignacion/menu_secundario.php
+++ asignacion/menu_secundario.php
@@ -83,6 +83,7 @@
 
 					<!-- <li><a href="stock_pdf.php" target="_blank" class="icon-file-pdf-o"> <span></span>Stock 0 KM</a></li> -->
 					<li><a href="stock_real_sin_vender_pdf.php" target="_blank" class="icon-file-pdf-o"> <span></span>Stock 0 KM - Real Sin Vender</a></li>
+					<li><a href="stock_real_sin_vender_mes_sig_pdf.php" target="_blank" class="icon-file-pdf-o"> <span></span>Stock 0 KM - Real Sin Vender Mes Siguiente</a></li>
 					<li><a href="stock_llegadas_sin_confirmar_pdf.php" target="_blank" class="icon-file-pdf-o"> <span></span>Stock 0 KM - Físico Sin Vender</a></li>
 					<li><a href="stock_real_sin_vender_con_interno.php" target="_blank" class="icon-file-pdf-o"> <span></span>Stock 0 KM - Sin Vender con interno</a></li>
 					<li><a href="stock_real_sin_vender_mes_ant_pdf.php" target="_blank" class="icon-file-pdf-o"> <span></span>Stock 0 KM (Mes Ant.)</a></li>
