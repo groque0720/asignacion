a0fc57023dc20259c3254064da83eefc848c13c9
Gómez Roque <32652492+groque0720@users.noreply.github.com>
2021-09-02 12:48:29 -0300

    R020920211248

---
+++ mistery/index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>GUIA MISTERY</title>
+	<meta charset="UTF-8" />
+  	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  	<link rel="shortcut icon" type="image/x-icon" href="../asignacion/imagenes/favicon.ico" />
+  	<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+
+</head>
+<body>
+	<div class="container m-auto">
+		<div class="flex items-center justify-center p-2 border-b">
+			<span class="text-xl text-indigo-400">GUIA ATENCION - PROCESO ESTILO COMERCIAL TOYOTA</span>
+		</div>
+		<div class="flex flex-col gap-2 md:flex-row  justify-between">
+			<div class="w-full bg-indigo-100 p-2">
+				<div class="">
+					<span class="text-red-600 font-bold">&#x23;</span>
+					<span class="text-lg">GUIA 0KM</span>
+				</div>
+				<div class="p-2">
+					<div class="mb-3">
+						<span class="text-red-600 font-bold">1 -</span>
+						<span class="text-lg font-bold">Atención Inicial</span>
+						<div class="pl-6 text-gray-600">
+							<p>Saludo inicial, identificación del concesionario y de la persona que atiende y oración de enlace orientada a la asistencia.</p>
+							<p class="text-sm text-gray-600 italic">Por Ejemplo: Buenos días/tardes/noches, Derka y Vargas, mi nombre es "nombre asesor", en que lo puedo ayudar/asesorar.</p>
+						</div>
+					</div>
+					<div class="mb-3">
+						<span class="text-red-600 font-bold">2 -</span>
+						<span class="text-lg font-bold">Detección de uso y necesidades</span>
+						<div class="pl-6 text-gray-600">
+							<p><span class="font-bold">a) - </span>El vendedor realiza una detección de uso y necesidades para poder ofrecer el producto y canal de ventas (convencional, usados, plan de ahorro) que mejor satisface al cliente.</p>
+							<p><span class="font-bold">b) - </span>Como resultado de una correcta detección de uso y necesidades, debemos conocer: uso del vehículo y vehículo actual del cliente.</p>
+							<p><span class="font-bold">c) - </span>Se deja registro de toda información adicional del cliente (familia, ocupación, preferencias, etc).</p>
+						</div>
+					</div>
+					<div class="mb-3">
+						<span class="text-red-600 font-bold">3 -</span>
+						<span class="text-lg font-bold">Presentación/Características del Producto</span>
+						<div class="pl-6 text-gray-600">
+							<p><span class="font-bold">a) - </span>(Presencial) La presentación de producto se realiza invitando al cliente a acercarse al vehículo en cuestión.</p>
+							<p><span class="font-bold">b) - </span>Se resaltan las características del vehículo de mayor interés para el cliente en función a las necesidades previamente indagadas.</p>
+							<div class="mt-2 p-1 border  border-red-500 flex flex-col items-center justify-center">
+								<p>Al 100% de los prospectos que ingresan por el canal digital, se les ofrece material adicional del vehículo elegido.</p>
+								<p class="text-sm text-gray-600 italic">Ejemplos: Link web, ficha técnica / catálogo (Pdf) - Whatsapp video - Youtube video</p>
+							</div>
+						</div>
+					</div>
+					<div class="mb-3">
+						<span class="text-red-600 font-bold">4 -</span>
+						<span class="text-lg font-bold">Valor de la Marca</span>
+						<div class="pl-6 text-gray-600">
+							<p><span class="font-bold">a) - </span>En algún momento de la presentación del vehículo, se resaltan los principales atributos de la marca Toyota: Garantía, respaldo de marca, calidad, servicio de posventa, valor de reventa de los usados, usados certificados, tecnología, fábrica en el país, etc.</p>
+							<p class="text-sm text-gray-600 italic">Sugerencia: a los nuevos clientes se les muestra físicamente las instalaciones del serviciode posventa del concesionario.</p>
+						</div>
+					</div>
+					<div class="mb-3">
+						<span class="text-red-600 font-bold">4 -</span>
+						<span class="text-lg font-bold">Ofrecimiento prueba de manejo</span>
+						<div class="pl-6 text-gray-600">
+							<p><span class="font-bold">a) - </span>Ofrecemos un test drive para demostrar las características relevantes a las necesidades detectadas en los interesados</p>
+						</div>
+						<div class="pl-6 text-gray-600">
+							<p><span class="font-bold">b) - </span>Ofrecer si o si, si no hay un modelo del interes del cliente mencionar que tenemos otros modelos que tiene la misma calidad del producto para que conozca.</p>
+						</div>
+					</div>
+					<div class="mb-3">
+						<span class="text-red-600 font-bold">5 -</span>
+						<span class="text-lg font-bold">Realización de Presupuesto</span>
+						<div class="pl-6 text-gray-600">
+							<p><span class="font-bold">a) - </span>Se confecciona un presupuesto de compra al 100% de los clientes interesados.</p>
+						</div>
+						<div class="pl-6 text-gray-600">
+							<p><span class="font-bold">b) - </span>Cierre de operación con prospecto digital o telefónico. El vendedor invita al prospecto digital al concesionario para intentar cerrar la operación.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+<!-- 			<div class="w-full md:w-1/2">
+				Usados
+			</div> -->
+		</div>
+
+	</div>
+</body>
+</html>
