33466ae07af21e2a1bd29423661e8a8ca2095f35
Gómez Roque <32652492+groque0720@users.noreply.github.com>
2022-12-14 12:08:46 -0300

    Update aa_leidas_notificaciones.php

--- asignacion/aa_leidas_notificaciones.php
+++ asignacion/aa_leidas_notificaciones.php
@@ -9,11 +9,11 @@
 	// Luis Gutierrez  = 94
 	// vargas Fredy = 41
 
-	$SQL="UPDATE notificaciones SET visto = 1 WHERE idusuario = 56";
-	mysqli_query($con, $SQL);
-
-	// $SQL="DELETE FROM notificaciones WHERE idnotificaciones < 685873";
+	// $SQL="UPDATE notificaciones SET visto = 1 WHERE idusuario = 56";
 	// mysqli_query($con, $SQL);
+
+	$SQL="DELETE FROM notificaciones WHERE idnotificaciones < 1146818";
+	mysqli_query($con, $SQL);
 
 	// $SQL="DELETE FROM sesiones WHERE id < 170464";
 	// mysqli_query($con, $SQL);
