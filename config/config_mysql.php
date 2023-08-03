4bfe48d265a2fb9854e45d564a01e4404719eb37
Roque Gómez <gomezroque@hotmail.com>
2023-08-03 16:23:50 -0300

    202308031623

--- config/config_mysql.php
+++ config/config_mysql.php
@@ -1,12 +1,12 @@
 <?php
 	//servidor
-	define('HOST','localhost');
-	//usuario bd
-	define('USER','root');
-	//pass
-	define('PASS','');
-	//base de datos
-	define('DB','asignacion');
+	// define('HOST','localhost');
+	// //usuario bd
+	// define('USER','root');
+	// //pass
+	// define('PASS','');
+	// //base de datos
+	// define('DB','asignacion');
 
 	//servidor
 	// define('HOST','localhost');
@@ -17,12 +17,12 @@
 	// //base de datos
 	// define('DB','asignacion');
 
-	// //servidor
-	// define('HOST','localhost');
-	// //usuario bd
-	// define('USER','usernavicat');
-	// //pass
-	// define('PASS','Belgrano872!');
-	// //base de datos
-	// define('DB','asignacion');
+	//servidor
+	define('HOST','localhost');
+	//usuario bd
+	define('USER','usernavicat');
+	//pass
+	define('PASS','Belgrano872!');
+	//base de datos
+	define('DB','asignacion');
 ?>
