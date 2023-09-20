b7fbe3a49737eed47eff4b2735f8791c7fe34b96
Roque <gomezroque@hotmail.com>
2023-09-20 11:38:11 -0300

    202309201138

--- asignacion/excel_planilla_asignacion_reventa_patente.php
+++ asignacion/excel_planilla_asignacion_reventa_patente.php
@@ -3,7 +3,7 @@
 // header('Content-Disposition: attachment; filename=nombre_archivo.xlsx');
 
 header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-header('Content-Disposition: attachment;filename="PlanillaAsignación_'.date("d-m-Y").'_'.date("H").date("i").date("s").'".xls"');
+header('Content-Disposition: attachment;filename="EstadoReventaPatente_'.date("d-m-Y").'_'.date("H").date("i").date("s").'".xls"');
 header('Cache-Control: max-age=0');
 
 
