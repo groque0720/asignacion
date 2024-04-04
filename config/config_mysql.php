6386b14e790aba931df607132b9fc79404211d7a
Gómez Roque <gomezroque@hotmail.com>
2024-04-04 10:53:07 -0300

    202404041053

---
+++ config/config_mysql.php
@@ -0,0 +1,19 @@
+<?php
+	// servidor
+	define('HOST','localhost');
+	//usuario bd
+	define('USER','root');
+	//pass
+	define('PASS','');
+	//base de datos
+	define('DB','asignacion');
+
+	// // servidor
+	// define('HOST','localhost');
+	// //usuario bd
+	// define('USER','usernavicat');
+	// //pass
+	// define('PASS','Belgrano872!');
+	// //base de datos
+	// define('DB','asignacion');
+?>
