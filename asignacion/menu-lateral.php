d384dadb4e5b75f5e03e0277b6dda438b5623239
Gómez Roque <gomezroque@hotmail.com>
2024-08-09 16:23:14 -0300

    deshabilitar link usados

--- asignacion/menu-lateral.php
+++ asignacion/menu-lateral.php
@@ -19,15 +19,15 @@ $cont_modelo = 1;
 					
 					// BLOQUE SOLO VISTO POR USARIOS ESPECIFICOS (Mauro vargas, Nicolas Burgos(89), Fredy Vargas (41) )
 					
-					// if($_SESSION["id"] == 56 || $_SESSION["id"] == 103 || $_SESSION["id"] == 41 || $_SESSION["id"] == 89 ) {
-					// 	$id_usado='link_usado';
-					// }else{
-					// 	$id_usado='bloqueado';
-					// }
+					if($_SESSION["id"] == 56 || $_SESSION["id"] == 103 || $_SESSION["id"] == 41 || $_SESSION["id"] == 89 ) {
+						$id_usado='link_usado';
+					}else{
+						$id_usado='bloqueado';
+					}
 					
 					// HABILITADOS PARA TODOS
 
-					$id_usado='link_usado';
+					// $id_usado='link_usado';
 
 				}
 			 ?>
