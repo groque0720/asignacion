932c01c25f46c3fbc217b37a5b0af15109f697e6
Gómez Roque <gomezroque@hotmail.com>
2025-01-09 18:17:29 -0300

    Mariano Api3

--- planes_avanzados/api/planes_mariano.php
+++ planes_avanzados/api/planes_mariano.php
@@ -7,7 +7,7 @@ mysqli_query($con, "SET NAMES 'utf8'");
 //COMPRUEBA QUE EL USUARIO ESTA AUTENTIFICADO
 if ($_SESSION["autentificado"] != "SI") {
     //si no existe, envio a la página de autentificacion
-    header("Location: ../login");
+    header("Location: /login");
     //ademas salgo de este script
     exit();
 }
