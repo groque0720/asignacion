1ef4f3c414924ef4ea963e3210cc66ed44631edf
Gómez Roque <gomezroque@hotmail.com>
2026-04-12 10:59:46 -0300

    fix(config): update BASE_URL_ENCUESTA to production URL

--- encuesta/config.php
+++ encuesta/config.php
@@ -6,7 +6,7 @@
 
 // URL base para generar los links de encuesta.
 // Cambiar en producción al dominio real.
-define('BASE_URL_ENCUESTA', 'http://localhost/encuesta/responder.php');
+define('BASE_URL_ENCUESTA', 'https://asignacion.derkayvargas.com/encuesta/responder.php');
 
 // Fecha mínima desde la que se traen entregas
 define('ENCUESTA_FECHA_DESDE', '2023/01/01');
