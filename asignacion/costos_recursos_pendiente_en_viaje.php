3a33ae66b52d102e9016b8086abec710962e3d75
Gómez Roque <gomezroque@hotmail.com>
2026-04-16 10:29:17 -0300

    feat: refine SQL queries for pending resource costs report

--- asignacion/costos_recursos_pendiente_en_viaje.php
+++ asignacion/costos_recursos_pendiente_en_viaje.php
@@ -101,9 +101,11 @@ $pdf->SetFont('');
 
 
 
-$SQL="SELECT * FROM view_asignaciones_saldo_pendiente_corregida WHERE Arribo IS NULL AND pagado_tasa IS true";
+// $SQL="SELECT * FROM view_asignaciones_saldo_pendiente_corregida WHERE Arribo IS NULL AND pagado_tasa IS true";
+$SQL="SELECT * FROM view_asignaciones_saldo_pendiente_corregida WHERE Arribo IS NULL AND pagado_tasa IS true AND  fec_arribo IS NULL";
 if (isset($_GET['sucursalId'])) {
-	$SQL="SELECT * FROM view_asignaciones_saldo_pendiente_corregida WHERE idsucursal = $sucursalId AND Arribo IS NULL AND pagado_tasa IS true";
+	// $SQL="SELECT * FROM view_asignaciones_saldo_pendiente_corregida WHERE idsucursal = $sucursalId AND Arribo IS NULL AND pagado_tasa IS true";
+	$SQL="SELECT * FROM view_asignaciones_saldo_pendiente_corregida WHERE idsucursal = $sucursalId AND Arribo IS NULL AND pagado_tasa IS true AND  fec_arribo IS NULL";
 }
 $unidades = mysqli_query($con, $SQL);
 
