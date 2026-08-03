59edb8109b9a0a51424d261d302da6053e066896
Gómez Roque <gomezroque@hotmail.com>
2026-08-03 16:59:30 -0300

    changePSW

--- asignacion/funciones/config_guard.php
+++ asignacion/funciones/config_guard.php
@@ -6,7 +6,7 @@
 	// Para cambiar la clave, generar el hash nuevo y reemplazar la constante:
 	//   php -r "echo password_hash('LA-CLAVE-NUEVA', PASSWORD_DEFAULT);"
 
-	define('GUARD_CLAVE_HASH', '$2y$10$CsCWDAzShHRbcANmLcww1.gDUAK3Q.3oXdO1f8C1iuHGCbOzJiRd2');
+	define('GUARD_CLAVE_HASH', '$2a$12$ygRqZpgeClYTp24K2DQU2eOOHeMxJwaLwv5lpsLpT8ChvlCFtD1AO');
 
 	// Minutos que dura la clave validada dentro de la misma sesión.
 	define('GUARD_MINUTOS', 15);
